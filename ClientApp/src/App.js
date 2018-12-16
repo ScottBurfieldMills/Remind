3ec302fda190378cd18a4b04b29f12b12088b08e
Scott Burfield-Mills <scott@scottbm.me>
2018-12-16 16:30:56 +1300

    Milestone - logging in correctly redirects user to homepage

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Router, Route } from 'react-router';
+import { connect } from 'react-redux';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { LoginPage } from './components/LoginPage';
+import { history } from './helpers';
+import { alertActions } from './actions';
 
-export default class App extends Component {
+/*export default */class App extends Component {
   static displayName = App.name;
+
+  constructor(props) {
+	  super(props);
+
+	  const { dispatch } = this.props;
+	  history.listen((location, action) => {
+		  // clear alert on location change
+		  dispatch(alertActions.clear());
+	  });
+  }
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Layout>
+            <Router history={history}>
+                <div>
+	                <Route exact path='/' component={Home} />
+	                <Route path='/counter' component={Counter} />
+	                <Route path='/fetch-data' component={FetchData} />
+                    <Route path='/login' component={LoginPage} />
+				</div>
+			</Router>
       </Layout>
     );
   }
 }
+
+function mapStateToProps(state) {
+	const { alert } = state;
+	return {
+		alert
+	};
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+export { connectedApp as App }; 
